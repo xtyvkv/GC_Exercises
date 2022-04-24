7fa393b62f8be7e52077b232f706ea3ad44f4ae8
Christy Hartner <xtyvkv@gmail.com>
2022-04-24 00:47:08 -0400

    re-upload after github issues

---
+++ Exercise_03/Exercise_03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a number.");
+string userInput = Console.ReadLine();
+double userDouble;
+if (double.TryParse(userInput, out userDouble))
+{
+    Console.WriteLine(userDouble + .5);
+}
+else
+{
+    Console.WriteLine("That is not a number.");
+}
