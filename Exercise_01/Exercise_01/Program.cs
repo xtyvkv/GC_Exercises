58989b304170de8383812378648106fa50340df1
Christy Hartner <xtyvkv@gmail.com>
2022-04-24 00:44:04 -0400

    re-upload after github issues

---
+++ Exercise_01/Exercise_01/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter some text:");
+string someText = Console.ReadLine();
+
+Console.WriteLine(someText);
---
+++ Exercise_02/Exercise_02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please enter a number.");
+string userNumberAnswer = Console.ReadLine();
+int userNumberInt;
+if (int.TryParse(userNumberAnswer, out userNumberInt))
+{
+    Console.WriteLine(userNumberInt + 1);
+}
+else
+{
+    Console.WriteLine("That is not a number.");
+}
+// path test fot github
