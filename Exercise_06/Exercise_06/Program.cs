6049c35513369036207e24150ca1db31ea53ee14
Christy Hartner <xtyvkv@gmail.com>
2022-04-24 01:51:24 -0400

    completed

---
+++ Exercise_06/Exercise_06/Program.cs
@@ -0,0 +1,24 @@
+﻿bool goAgain = true;
+// string input;
+
+while (goAgain)
+{
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Would you like to continue (y/n)?");
+    string input = Console.ReadLine();
+    if (input == "y" || input == "Y")
+    {
+        goAgain = true;
+    }
+    else if (input == "n" || input == "N")
+    {
+        goAgain = false;
+        Console.WriteLine("Goodbye!");
+    }
+    else
+    {
+        goAgain = false;
+        Console.WriteLine("Invalid input. Goodbye!");
+        // try asking to again again after invalid input, without repeating "Hello, World!"
+    }
+}
