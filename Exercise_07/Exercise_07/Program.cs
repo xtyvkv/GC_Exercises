23cf007278529f72d75d0f573809973656a60c73
Christy Hartner <xtyvkv@gmail.com>
2022-04-24 02:04:06 -0400

    completed, but additional work can be done

---
+++ Exercise_07/Exercise_07/Program.cs
@@ -0,0 +1,26 @@
+﻿bool goAgain = true;
+// i thought without saying true it assumes true^?
+
+while(goAgain)
+{
+    Console.WriteLine("Enter some text:");
+    string someText = Console.ReadLine();
+    Console.WriteLine(someText);
+    Console.WriteLine("Would you like to continue (y/n)?");
+    string userWannaGo = Console.ReadLine();
+    if (userWannaGo == "y" || userWannaGo == "Y")
+    {
+        goAgain = true;
+    }
+    else if (userWannaGo == "n" || userWannaGo == "N")
+    {
+        goAgain = false;
+        Console.WriteLine("Goodbye!");
+    }
+    else
+    {
+        goAgain = false;
+        Console.WriteLine("Invalid input. Goodbye!");
+        // try asking to again again after invalid input, without repeating "Hello, World!"
+    }
+}    
