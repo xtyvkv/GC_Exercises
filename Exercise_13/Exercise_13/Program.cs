1a17abf22832cbca19b4a2b4fa4b0a62d5032377
Christy Hartner <xtyvkv@gmail.com>
2022-04-25 02:01:48 -0400

    completed, see notes...again

---
+++ Exercise_13/Exercise_13/Program.cs
@@ -0,0 +1,34 @@
+﻿bool goAgain = true;
+while (goAgain == true)
+{
+    Console.WriteLine("Enter a number.");
+    string input = Console.ReadLine();
+    int inputInt = int.Parse(input);
+    Console.WriteLine();
+    for (int i = inputInt; i >= 0; i--)
+    {
+        Console.WriteLine(i);
+    }
+    Console.WriteLine();
+    Console.WriteLine("Would you like to continue (y/n)?");
+    string goAgainAnswer = Console.ReadLine();
+    if (goAgainAnswer == "y" || goAgainAnswer == "Y")
+    {
+        Console.WriteLine();
+        continue;
+    }
+    else if (goAgainAnswer == "n" || goAgainAnswer == "N")
+    {
+        Console.WriteLine();
+        break;
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Invalid answer.");
+        break;
+    }
+}
+Console.WriteLine("Goodbye!");
+
+// same thing with exercise 11 and 12...done but figure out that thing.
